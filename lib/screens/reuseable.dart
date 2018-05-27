cd0a7c055c9451b62be9d11e44bfcdc8b9e0f6c2
RBayor <rbayor16@gmail.com>
2018-05-27 00:12:14 +0000

    modified appbar header color

--- lib/screens/reuseable.dart
+++ lib/screens/reuseable.dart
@@ -3,21 +3,20 @@ import 'package:flutter/material.dart';
 class MyAppBar extends AppBar {
   MyAppBar({Key key, Widget title})
       : super(
-        key: key, 
-        title: title,
-        centerTitle: true,
-        backgroundColor: Colors.green, 
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.more_vert),
-            onPressed: () {},
-          ),
-        ]);
+            key: key,
+            title: title,
+            centerTitle: true,
+            backgroundColor: Colors.green,
+            actions: <Widget>[
+              new IconButton(
+                icon: new Icon(Icons.more_vert),
+                onPressed: () {},
+              ),
+            ]);
 }
 
 class MyDrawer extends Drawer {
-
-    final appBarLogo = Hero(
+  final appBarLogo = Hero(
     tag: 'hero',
     child: CircleAvatar(
       backgroundColor: Colors.transparent,
@@ -26,61 +25,79 @@ class MyDrawer extends Drawer {
     ),
   );
 
-  MyDrawer({Key key, Widget child, BuildContext context}) 
-  : super(
-    key : key,
-    child: ListView(
-      children: <Widget>[
-        UserAccountsDrawerHeader(
-          accountEmail: null,
-          accountName: null,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.contain,
-              image: AssetImage('images/appbar_icon.png'
-              )
-            )
+  MyDrawer({Key key, Widget child, BuildContext context})
+      : super(
+          key: key,
+          child: ListView(
+            //addRepaintBoundaries: true,
+            children: <Widget>[
+              Container(
+                /**
+                 * 
+                 * 
+                 * 
+                 * 
+                 * Bandoh delete this and work with the color here
+                 * 
+                 * 
+                 * 
+                 * 
+                 * 
+                 */
+                color: Colors.green,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    UserAccountsDrawerHeader(
+                      accountEmail: null,
+                      accountName: null,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.contain,
+                              image: AssetImage('images/appbar_icon.png'))),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ListTile(
+                title: Text('Home'),
+                leading: Icon(Icons.home),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pushNamed('/Home');
+                },
+              ),
+              ListTile(
+                title: Text('Health Tips'),
+                leading: Icon(Icons.help),
+              ),
+              ListTile(title: Text('Maps'), leading: Icon(Icons.map)),
+              ListTile(
+                title: Text('Pharmacies'),
+                leading: Icon(Icons.local_pharmacy),
+              ),
+              Divider(
+                height: 20.0,
+              ),
+              ListTile(
+                  title: Text('Terms of Service'), leading: Icon(Icons.info)),
+              ListTile(
+                  title: Text('About Us'), leading: Icon(Icons.local_pharmacy)),
+              ListTile(title: Text('Settings'), leading: Icon(Icons.settings)),
+              Divider(
+                height: 10.0,
+              ),
+              ListTile(
+                  title: Text('Donate'), leading: Icon(Icons.monetization_on)),
+            ],
           ),
-        ),
-        ListTile(
-          title: Text('Home'),
-          leading: Icon(Icons.home),
-          onTap: (){
-            Navigator.of(context).pop();
-            Navigator.of(context).pushNamed('Home');
-          },
-        ),
-        ListTile(
-          title: Text('Health Tips'),
-          leading: Icon(Icons.help),
-        ),
-        ListTile(
-          title: Text('Maps'),
-          leading: Icon(Icons.map)
-        ),
-        ListTile(
-          title: Text('Pharmacies'),
-          leading: Icon(Icons.local_pharmacy),
-        ),
-        Divider(height: 20.0,),
-        ListTile(
-          title: Text('Terms of Service'),
-          leading: Icon(Icons.info)
-        ),
-        ListTile(
-          title: Text('About Us'),
-          leading: Icon(Icons.local_pharmacy)
-        ),
-        ListTile(
-          title: Text('Settings'),
-          leading: Icon(Icons.settings)
-        ),
-        Divider(height: 10.0,),
-        ListTile(
-          title: Text('Donate'),
-          leading: Icon(Icons.monetization_on)
-        ),
-      ],
-    ),
-  );
+        );
 }
