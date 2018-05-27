d309a35b45687aa9e77b95e670903f61cd5d689b
Bandoh <kelvinbandoh@hotmail.com>
2018-05-27 22:04:29 +0000

    Created Dummy Structure Of Database

---
+++ lib/firebase/MainFire.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MainFire {
+
+  /**
+   * These are all dummy data 
+   */
+  createpharmdet() {
+    CollectionReference cr = Firestore.instance.collection("PharmacyDetails");
+    cr.add({
+      "Name": "BB",
+      "E-mail": "something@gh.com",
+      "PhoneNumber": 2233345667,
+      "Location": "Kumasi",
+      "Stock": ["Panadol", "Tramadol"]
+    }).whenComplete(() {
+      print("Saved Pharmacy Details");
+    });
+  }
+
+  createownerdet() {
+    CollectionReference cr = Firestore.instance.collection("OwnerDeatils");
+    cr.add({
+      "Name": "BB",
+      "E-mail": "something@gh.com",
+      "PhoneNumber": 2233345667,
+      "Owns": "Tech Hostpital"
+    }).whenComplete(() {
+      print("Saved Owner Details");
+    });
+  }
+}
