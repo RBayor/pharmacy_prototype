7f94967ccab5b98ea4b34e3f7cfb895fff71c185
Bandoh <kelvinbandoh@hotmail.com>
2018-06-04 20:32:06 +0000

    Added Favorite Icon

--- lib/screens/health.dart
+++ lib/screens/health.dart
@@ -17,22 +17,38 @@ class _HealthState extends State<Health> {
   }
 
   var data;
-  final String url = "https://newsapi.org/v2/top-headlines?sources=medical-news-today&apiKey=afd62a89063d49509471031e7f44c047";
+  final String url =
+      "https://newsapi.org/v2/top-headlines?sources=medical-news-today&apiKey=afd62a89063d49509471031e7f44c047";
 
   _fetchData() async {
     print("fetching data from network");
 
     http.Response response = await http
         .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
-      
+
     setState(() {
       _isLoading = false;
       this.data = json.decode(response.body);
       //print(data['articles'][2]['author']);
     });
-    
   }
-  
+
+  List<bool> fav = [];
+  _favnewcount(int id) {
+    for (int i = 0; i <= id; i++) {
+      fav.add(false);
+    }
+  }
+
+  favchange(int i) {
+    setState(() {
+      if (fav[i]) {
+        fav[i] = false;
+      } else {
+        fav[i] = true;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,8 +73,11 @@ class _HealthState extends State<Health> {
             child: _isLoading == true
                 ? CircularProgressIndicator()
                 : ListView.builder(
-                    itemCount: data['articles'].length != null ? data['articles'].length : 0,
+                    itemCount: data['articles'].length != null
+                        ? data['articles'].length
+                        : 0,
                     itemBuilder: (BuildContext context, int index) {
+                      _favnewcount(data['articles'].length);
                       return Card(
                         elevation: 5.0,
                         margin: EdgeInsets.only(
@@ -66,28 +85,56 @@ class _HealthState extends State<Health> {
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           children: <Widget>[
-                            Image.network(data['articles'][index]['urlToImage']),
+                            Image
+                                .network(data['articles'][index]['urlToImage']),
                             new Padding(
                               padding: const EdgeInsets.only(
-                                left: 30.0,
-                                right: 10.0,
-                                top: 2.0,
-                                bottom: 5.0
-                              ),
+                                  left: 30.0,
+                                  right: 10.0,
+                                  top: 2.0,
+                                  bottom: 5.0),
                               child: ListTile(
-                                 //Image.network(data['articles'][index]['urlToImage']),
-                                title: Text(
-                                  '${data['articles'][index]['title']}',
-                                  style: TextStyle(
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.bold
+                                  //Image.network(data['articles'][index]['urlToImage']),
+                                  title: Text(
+                                    '${data['articles'][index]['title']}',
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
                                   ),
+                                  subtitle: Text(
+                                      '${data['articles'][index]['description']}'),
+                                  onTap:
+                                      () {}, // We will open the full article here for reading,
+                                  onLongPress: () {
+                                    favchange(index);
+                                  } // This add article to favorites,
                                   ),
-                                subtitle: Text('${data['articles'][index]['description']}'),
-                                onTap: (){}, // We will open the full article here for reading,
-                                onLongPress: (){} // This add article to favorites,
-                              ),
                             ),
+                            new Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                new IconButton(
+                                    icon: new Icon(
+                                      Icons.share,
+                                      color: Colors.cyan,
+                                    ),
+                                    onPressed: () {}),
+                                new IconButton(
+                                  icon: (fav[index])
+                                      ? new Icon(
+                                          Icons.favorite,
+                                          color: Colors.red,
+                                        )
+                                      : new Icon(
+                                          Icons.favorite_border,
+                                          color: Colors.red,
+                                        ),
+                                  onPressed: () {
+                                    favchange(index);
+                                  },
+                                )
+                              ],
+                            )
                           ],
                         ),
                       );
